10ad8fd9796a777635ad6e73e6570326ba4b9baa
maxime personnic <maxime.personnic@hotmail.fr>
2019-09-29 09:59:11 +0200

    add some gems

--- ruby/rbenv.sh
+++ ruby/rbenv.sh
@@ -12,3 +12,6 @@ rbenv install 2.6.3
 
 # Set default version ruby
 rbenv global 2.6.3
+
+# Installing default gems
+gem install rake bundler rspec rubocop rubocop-performance pry pry-byebug hub colored octokit rails awesome_print
