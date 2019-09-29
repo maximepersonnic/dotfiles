785c0681e73d9b98392bb3497129cbaea1931701
maxime personnic <maxime.personnic@hotmail.fr>
2019-09-29 16:22:13 +0200

    update sublime.sh

--- sublime/sublime.sh
+++ sublime/sublime.sh
@@ -8,16 +8,13 @@ backup() {
   fi
 }
 
-if [[ ! `uname` =~ "darwin" ]]; then
-  SUBL_PATH=~/.config/sublime-text-3
-else
-  SUBL_PATH=~/Library/Application\ Support/Sublime\ Text\ 3
-fi
+SUBL_PATH=~/Library/Application\ Support/Sublime\ Text\ 3
+
 mkdir -p $SUBL_PATH/Packages/User $SUBL_PATH/Installed\ Packages
 backup "$SUBL_PATH/Packages/User/Preferences.sublime-settings"
 curl -k https://sublime.wbond.net/Package%20Control.sublime-package > $SUBL_PATH/Installed\ Packages/Package\ Control.sublime-package
-ln -s $PWD/sublime/Preferences.sublime-settings $SUBL_PATH/Packages/User/Preferences.sublime-settings
-ln -s $PWD/sublime/Package\ Control.sublime-settings $SUBL_PATH/Packages/User/Package\ Control.sublime-settings
-ln -s $PWD/sublime/SublimeLinter.sublime-settings $SUBL_PATH/Packages/User/SublimeLinter.sublime-settings
+ln -s $PWD/sublime/Preferences.sublime-settings "$SUBL_PATH/Packages/User/Preferences.sublime-settings"
+ln -s $PWD/sublime/Package\ Control.sublime-settings "$SUBL_PATH/Packages/User/Package\ Control.sublime-settings"
+ln -s $PWD/sublime/SublimeLinter.sublime-settings "$SUBL_PATH/Packages/User/SublimeLinter.sublime-settings"
 
 zsh ~/.zshrc
